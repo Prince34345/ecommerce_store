c2ed70ca7ec7b1e318f9d3ce4c86adfef4512460
Prince34345 <priyanshudhawan55@gmail.com>
2025-05-13 12:23:14 +0530

    add usecart hook

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -10,6 +10,7 @@ import CurrencyComponent from "./currency-component";
 import { useRouter } from "next/navigation";
 import { MouseEventHandler } from "react";
 import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCardProps {
     data: Product
@@ -18,6 +19,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({
     data
 }) => {
+  const cart = useCart();
   const previewModal = usePreviewModal();
   const router = useRouter();
   const  handleClick = () => {
@@ -30,6 +32,13 @@ const ProductCard: React.FC<ProductCardProps> = ({
       previewModal.onOpen(data);
   }
 
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+}
+
   return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl shadow-md shadow-gray-300 p-3 space-y-4">
        <div className="aspect-square rounded-xl bg-gray-50 relative">
@@ -37,7 +46,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
           <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
             <div className="flex gap-x-6 justify-center">
                 <IconButton onClick={onPreview} icon={<Expand size={20} className="text-gray-600"/>}/>
-                <IconButton onClick={() => {}} icon={<ShoppingCartIcon size={20} className="text-gray-600"/>}/>
+                <IconButton onClick={onAddToCart} icon={<ShoppingCartIcon size={20} className="text-gray-600"/>}/>
             </div>
           </div>
        </div>
